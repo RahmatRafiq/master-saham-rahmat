15eae553dc7d137dfd5593186dd1a6ae2b4172c6
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2023-07-19 02:26:41 +0800

    API add to dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Api\ApiController;
+
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('backend.layouts.dashboard');
+        $apiController = new ApiController();
+        $response = $apiController->index();
+        $content = $response->getContent();
+        $stocks = json_decode($content, true);
+
+        $count = $stocks['data']['count'];
+
+        return view('backend.layouts.dashboard', ['stocks' => $stocks, 'count' => $count]);
     }
 }
--- resources/views/backend/includes/body.blade.php
+++ resources/views/backend/includes/body.blade.php
@@ -25,10 +25,11 @@
              <li>
                  <i class='bx bxs-calendar-check'></i>
                  <span class="text">
-                     <h3>1020</h3>
+                     <h3>{{ $count }}</h3>
                      <p>Jumlah Komoditas</p>
                  </span>
              </li>
+
              <li>
                  <i class='bx bxs-group'></i>
                  <span class="text">
@@ -49,61 +50,32 @@
          <div class="table-data">
              <div class="order">
                  <div class="head">
-                     <h3>Recent Orders</h3>
+                     <h3>List Komoditas</h3>
                      <i class='bx bx-search'></i>
                      <i class='bx bx-filter'></i>
                  </div>
+
                  <table>
                      <thead>
                          <tr>
-                             <th>User</th>
-                             <th>Date Order</th>
-                             <th>Status</th>
+                             <th>Ticker</th>
+                             <th>Name</th>
+                             <th>Logo</th>
                          </tr>
                      </thead>
                      <tbody>
-                         <tr>
-                             <td>
-                                 <img src="img/people.png">
-                                 <p>John Doe</p>
-                             </td>
-                             <td>01-10-2021</td>
-                             <td><span class="status completed">Completed</span></td>
-                         </tr>
-                         <tr>
-                             <td>
-                                 <img src="img/people.png">
-                                 <p>John Doe</p>
-                             </td>
-                             <td>01-10-2021</td>
-                             <td><span class="status pending">Pending</span></td>
-                         </tr>
-                         <tr>
-                             <td>
-                                 <img src="img/people.png">
-                                 <p>John Doe</p>
-                             </td>
-                             <td>01-10-2021</td>
-                             <td><span class="status process">Process</span></td>
-                         </tr>
-                         <tr>
-                             <td>
-                                 <img src="img/people.png">
-                                 <p>John Doe</p>
-                             </td>
-                             <td>01-10-2021</td>
-                             <td><span class="status pending">Pending</span></td>
-                         </tr>
-                         <tr>
-                             <td>
-                                 <img src="img/people.png">
-                                 <p>John Doe</p>
-                             </td>
-                             <td>01-10-2021</td>
-                             <td><span class="status completed">Completed</span></td>
-                         </tr>
+                         @foreach ($stocks['data']['results'] as $result)
+                             <tr>
+                                 <td><strong>{{ $result['ticker'] }}</strong></td>
+                                 <td>{{ $result['name'] }}</td>
+                                 <td>
+                                     <img src="{{ $result['logo'] }}" alt="Company Logo">
+                                 </td>
+                             </tr>
+                         @endforeach
                      </tbody>
                  </table>
+
              </div>
              <div class="todo">
                  <div class="head">

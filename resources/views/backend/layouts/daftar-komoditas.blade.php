4f5713dafde2fda8873c18313c1ee28c3d20fc7c
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2023-07-23 15:41:02 +0800

    jangan disentuh

--- resources/views/backend/layouts/daftar-komoditas.blade.php
+++ resources/views/backend/layouts/daftar-komoditas.blade.php
@@ -1,6 +1,5 @@
 @extends('backend.layouts.backend')
-@section('content')
-    @include('backend.includes.head')
-    @include('backend.includes.sidebar')
-    @include('backend.includes.js')
-@endsection
+
+@include('backend.includes.head')
+@include('backend.includes.sidebar')
+@include('backend.includes.js')

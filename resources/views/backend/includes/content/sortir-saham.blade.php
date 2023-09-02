00fe7a20ffc5b7df8f034df95e0a818f4a314f75
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2023-09-02 21:26:12 +0800

    add modals

--- resources/views/backend/includes/content/sortir-saham.blade.php
+++ resources/views/backend/includes/content/sortir-saham.blade.php
@@ -43,7 +43,9 @@
                                     Waktu : {{ $dataSortir->created_at->format('d-m-Y') }}
                                 </p>
                             </td>
-                            <i class='bx bx-dots-vertical-rounded'></i>
+                            <td>
+                                <a href="#modal-1"><i class='bx bx-dots-vertical-rounded'></i></a>
+                            </td>
                         </li>
                     @endforeach
                 </ul>
@@ -58,25 +60,50 @@
 
                     @foreach ($hasilSortir as $hasil)
                         <li class="completed">
-                            <p><strong>
-                                    {{ $hasil->symbol }}
-                                </strong>
-                                <br>
-                                Harga Terendah Sebelumnya = Rp {{ $hasil->open }}
-                                <br>
-                                Harga Tutup Hari Ini = Rp {{ $hasil->high }}
-                                <br>
-                                <br>
-                                Waktu : {{ $hasil->created_at }}
-                            </p>
-                            <i class='bx bx-dots-vertical-rounded'></i>
+                            <td>
+                                <p><strong>
+                                        {{ $hasil->symbol }}
+                                    </strong>
+                                    <br>
+                                    Harga Terendah Sebelumnya = Rp {{ $hasil->open }}
+                                    <br>
+                                    Harga Tutup Hari Ini = Rp {{ $hasil->high }}
+                                    <br>
+                                    <br>
+                                    Waktu : {{ $hasil->created_at }}
+                                </p>
+                            </td>
+                            <td>
+                                <a href="#modal-1"><i class='bx bx-dots-vertical-rounded'></i></a>
+                            </td>
                         </li>
                     @endforeach
                 </ul>
             </div>
             <a href="{{ route('Process and Sort Stocks Daily') }}" class="btn btn-primary">Process and Sort Stocks</a>
-
         </div>
-    </main>
-    <!-- MAIN -->
+        <div id="modal-1">
+            <a href="#" class="modal__bg"></a>
+            <div class="modal__content small-modal">
+                <div>
+                    <h1 class="content__title">
+                        Update data saham?
+                    </h1>
+                    <div class="modal-buttons">
+                        <button class="update-button">Iya</button>
+                    </div>
+                    <div class="content__btn-close">
+                        <a href="#">
+                            <!-- href disi untuk route untuk request ulang   -->
+
+                            <i class="fa fa-times" aria-hidden="true"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
+
+</main>
+<!-- MAIN -->
 </section>
